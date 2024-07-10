3a0a5feed3d834ca437690190fadc56043dbe34b
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-07-10 11:34:43 +0100

    fix errors

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -1,9 +1,5 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 import { InitAuth0 } from '@auth0/nextjs-auth0';
 
-export default handleAuth(
-    {
-        scope: 'openid profile email' 
-    }
-);
+export default handleAuth();
 
