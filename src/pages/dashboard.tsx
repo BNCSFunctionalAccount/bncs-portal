8de0fcb877c9d85bf7c2336b2ac646b499e3bcb6
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-08-28 09:56:04 +0100

    license filter

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,15 +2,12 @@ import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { useLiveQuery } from 'next-sanity/preview';
-import Container from '~/components/Container';
-import Welcome from '~/components/Welcome';
 import { readToken } from '~/lib/sanity.api';
 import { getClient } from '~/lib/sanity.client';
 import { getPosts, type Post, postsQuery } from '~/lib/sanity.queries';
 import type { SharedPageProps } from '~/pages/_app';
 import styles from '../styles/dashboard.module.css';
 import Sidebar from '~/components/sidebar';
-import Link from 'next/link';
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
 // Function to check if user has access to download for a specific driver
@@ -41,6 +38,7 @@ const IndexPage = (
   const [posts] = useLiveQuery<Post[]>(props.posts, postsQuery);
   const { user, isLoading, error } = useUser();
   const [userRoles, setUserRoles] = useState<string[]>([]);
+  const [filter, setFilter] = useState<string>('All'); // Filter state
 
   useEffect(() => {
     if (user && user["https://localhost:3000/roles"]) {
@@ -48,6 +46,13 @@ const IndexPage = (
       setUserRoles(roles);
     }
   }, [user]);
+
+  const filteredPosts = posts.filter(post => {
+    const isLicensed = userCanDownload(userRoles, post.roles);
+    if (filter === 'Licensed') return isLicensed;
+    if (filter === 'Not Licensed') return !isLicensed;
+    return true; // 'All' case
+  });
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -57,11 +62,20 @@ const IndexPage = (
       <Head>
         <title>Drivers</title>
       </Head>
+    
       <Sidebar />
       <div className={styles.content}>
-        <h2>Dashboard</h2>
+      
+        <div className={styles.filterContainer}>
+          <label htmlFor="filter">License: </label>
+          <select id="filter" value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">All</option>
+            <option value="Licensed">Licensed</option>
+            <option value="Not Licensed">Not Licensed</option>
+          </select>
+        </div>
         <section>
-          {posts.length ? (
+          {filteredPosts.length ? (
             <table className={styles.table}>
               <thead>
                 <tr>
@@ -72,7 +86,7 @@ const IndexPage = (
                 </tr>
               </thead>
               <tbody>
-                {posts.map((post) => (
+                {filteredPosts.map((post) => (
                   <tr key={post._id}>
                     <td>{post.title}</td>
                     <td>{post.version}</td>
