b117b90075be49d6c95b15c8d1cf058581fc83f4
luke-dcz <lukedacruz@icloud.com>
2024-09-18 17:14:36 +0100

    add tailwind css configs

---
+++ src/lib/fonts.ts
@@ -0,0 +1,20 @@
+import { IBM_Plex_Mono, Inter, PT_Serif } from "next/font/google";
+
+export const mono = IBM_Plex_Mono({
+	variable: '--font-family-mono',
+	subsets: ['latin'],
+	weight: ['500', '700'],
+  });
+  
+ export const sans = Inter({
+	variable: '--font-family-sans',
+	subsets: ['latin'],
+	weight: ['500', '700', '800'],
+  });
+  
+export  const serif = PT_Serif({
+	variable: '--font-family-serif',
+	style: ['normal', 'italic'],
+	subsets: ['latin'],
+	weight: ['400', '700'],
+  });
