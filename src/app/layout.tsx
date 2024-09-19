1e71ee417dc25409fc2f9e1b853358e36de49b90
luke-dcz <lukedacruz@icloud.com>
2024-09-19 15:38:53 +0100

    add dashboard provider to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import { Metadata } from "next"
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { sans } from "~/lib/fonts";
+import { DashboardProvider } from '~/lib/DashboardProvider';
 
 const PreviewProvider = dynamic(() => import('~/components/PreviewProvider'))
 
@@ -26,13 +27,15 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={`${sans.className}`}>
 				<UserProvider>
-					{draftMode ? (
-						<PreviewProvider token={token}>
-							{children}
-						</PreviewProvider>
-					) : (
-						children
-					)}
+					<DashboardProvider>
+						{draftMode ? (
+							<PreviewProvider token={token}>
+								{children}
+							</PreviewProvider>
+						) : (
+							children
+						)}
+					</DashboardProvider>
 				</UserProvider>
 			</body>
 		</html >
