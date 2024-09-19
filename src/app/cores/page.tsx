37c968a111d2e25aec141c946ccde6493375cc66
luke-dcz <lukedacruz@icloud.com>
2024-09-19 17:00:34 +0100

    create cores page

---
+++ src/app/cores/page.tsx
@@ -0,0 +1,35 @@
+import { getClient } from "~/lib/sanity.client"
+import { getPosts } from "~/lib/sanity.queries"
+import styles from '../../styles/dashboard.module.css'
+import Sidebar from "~/components/Sidebar"
+import { Filter } from "~/components/Filter"
+import { Search } from "~/components/Search"
+import { CoresTable } from "~/components/CoresTable"
+import { CoresProvider } from "~/lib/CoresProvider"
+
+async function getStaticPosts() {
+	const client = getClient()
+	const posts = await getPosts(client)
+
+	return posts
+}
+
+export default async function Cores() {
+	const staticPosts = await getStaticPosts()
+
+	return (
+		<div className={styles.dashboard}>
+			<Sidebar />
+			<div className={styles.content}>
+				<h2>Cores</h2>
+				<CoresProvider>
+					<div className={styles.filterContainer}>
+						<Filter />
+						<Search />
+					</div>
+					<CoresTable staticPosts={staticPosts} />
+				</CoresProvider>
+			</div>
+		</div>
+	)
+}
