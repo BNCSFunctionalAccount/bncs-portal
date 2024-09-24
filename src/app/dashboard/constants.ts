0d72ed3db07ba3ee409834a724c942c615d46895
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-09-24 16:50:05 +0100

    Added consts for headers

---
+++ src/app/dashboard/constants.ts
@@ -0,0 +1,8 @@
+import { IHeader } from '~/components/table/types'
+
+export const DRIVER_HEADERS: IHeader[] = [
+  { title: 'Title', width: '250px' },
+  { title: 'Version', width: '100px' },
+  { title: 'Description', width: '550px' },
+  { title: 'License', width: '90px' },
+]
