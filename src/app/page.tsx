aca8c773f36b03a641d0685f1fa12bfb9ca8891b
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-09-24 11:09:46 +0100

    revert bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from '../components/header'
+import { Header } from '../components/Header'
 import styles from '../styles/landingPage.module.css'
 
 export default function LandingPage() {
