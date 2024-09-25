f6dcc0aab14dc164a83087b144c0cfbdd477d4a7
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-09-25 15:39:17 +0100

    add eviden branding to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { DashboardSearch } from '../../components/DashboardSearch'
 import { DashboardTable } from '../../components/DashboardTable'
 import { DashboardProvider } from '../../lib/providers/DashboardProvider'
 import { getStaticPosts } from '~/utils'
+import logo from '../../assets/logo.png'
+import Image from 'next/image'
 
 export default async function Dashboard() {
   const staticPosts = await getStaticPosts()
@@ -13,6 +15,14 @@ export default async function Dashboard() {
       <Sidebar />
       <div className="ml-72 p-5 w-[calc(100%-260px)]">
         <h2>Dashboard</h2>
+        <div className='absolute right-6 top-10'>
+          <Image
+            src={logo}
+            alt='Eviden Logo'
+            width={175}
+            height={100}
+          />
+        </div>
         <DashboardProvider>
           <div className="flex gap-4 items-center mb-5">
             <DashboardFilter />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,29 +2,31 @@ import Link from 'next/link'
 
 export const Sidebar = () => {
   return (
-    <div className="w-64 h-full bg-[#f0f0f0] p-5 fixed shadow-md">
-      <h3 className='font-bold mb-5'>BNCS Product Portal</h3>
-      <ul>
+    <div className="w-64 h-full bg-deepBlue text-white p-5 fixed shadow-md">
+      <div className='flex flex-col gap-4 mb-5'>
+        <h3 className='font-semibold text-xl'>BNCS Product Portal</h3>
+      </div>
+      <ul className='text-lg'>
         <li className='mb-2'>
-          <Link className='mr-5 hover:text-[#333] hover:font-bold transition-all ease-in-out duration-150' href="/">
+          <Link className='mr-5 hover:text-evidenOrange hover:font-bold transition-all ease-in-out duration-150' href="/">
             Home
           </Link>
         </li>
         <li className='mb-2'>
-          <Link className='mr-5 hover:text-[#333] hover:font-bold transition-all ease-in-out duration-150' href="/dashboard">
+          <Link className='mr-5 hover:text-evidenOrange hover:font-bold transition-all ease-in-out duration-150' href="/dashboard">
             Dashboard
           </Link>
         </li>
         <li className='mb-2'>
           <Link
-            className='mr-5 hover:text-[#333] hover:font-bold transition-all ease-in-out duration-150'
+            className='mr-5 hover:text-evidenOrange hover:font-bold transition-all ease-in-out duration-150'
             href="https://bncs.atlassian.net/servicedesk/customer/portal/1/user/login?destination=portal%2F1"
           >
             Submit a Ticket
           </Link>
         </li>
         <li className='mb-2'>
-          <Link className='mr-5 hover:text-[#333] hover:font-bold transition-all ease-in-out duration-150' href="/api/auth/logout">
+          <Link className='mr-5 hover:text-evidenOrange hover:font-bold transition-all ease-in-out duration-150' href="/api/auth/logout">
             Logout
           </Link>
         </li>
