cd40c3b8ab7eae533a71fae983d8a32885e08d3f
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-09-26 09:49:22 +0100

    Replace existing select with new component

--- src/components/DashboardFilter.tsx
+++ src/components/DashboardFilter.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useContext } from 'react'
+
 import { DashboardContext } from '~/lib/providers/DashboardProvider'
+
 import { Select } from './select'
 
 export const DashboardFilter = () => {
@@ -9,22 +11,14 @@ export const DashboardFilter = () => {
 
   return (
     <div className="flex gap-2 items-center">
-      <label htmlFor="filter">Filter License: </label>
-      <select
-        id="filter"
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
-        className="border p-1 border-black rounded"
-      >
-        <option value="All">All</option>
-        <option value="Licensed">Licensed</option>
-        <option value="Not Licensed">Not Licensed</option>
-      </select>
       <Select
-        titleText="Filter License"
+        selectedOption={filter}
+        setOption={setFilter}
+        border
+        titleText={'Filter License'}
         menuItems={[
-          { title: 'All', clickEffect: () => console.log('title') },
-          { title: 'Licensed', clickEffect: (e) => console.log(e.target) },
+          { title: 'All' },
+          { title: 'Licensed' },
           { title: 'Not Licensed', clickEffect: (e) => console.log(e.target) },
         ]}
       />
