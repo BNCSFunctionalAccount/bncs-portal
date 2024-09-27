c147bdfab53a5e2a7cfb3d0ddaa524e820b3a311
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-09-27 13:04:27 +0100

    Add release version alongside release note

--- src/components/slug-section/sub-components/release-notes.tsx
+++ src/components/slug-section/sub-components/release-notes.tsx
@@ -1,6 +1,7 @@
 import { PortableText } from '@portabletext/react'
 import { FC } from 'react'
 import { PortableTextBlock } from 'sanity'
+
 import { serif } from '~/assets/fonts'
 import { Post } from '~/lib/sanity.queries'
 
@@ -9,14 +10,17 @@ interface IReleaseNotesProps {
 }
 
 export const ReleaseNotes: FC<IReleaseNotesProps> = ({
-  post: { releaseNotes },
+  post: { releaseNotes, version },
 }) => (
-  <div className={`${serif.className} text-xl tracking-tight mt-6`}>
-    {/* post.releaseNotes should be of type PortableTextBlock but is currently returning as string instead */}
-    {typeof releaseNotes == 'string' ? (
-      releaseNotes
-    ) : (
-      <PortableText value={releaseNotes} />
-    )}
+  <div className="mt-5">
+    Version {version}
+    <div className={`${serif.className} text-xl tracking-tight`}>
+      {/* post.releaseNotes should be of type PortableTextBlock but is currently returning as string instead */}
+      {typeof releaseNotes == 'string' ? (
+        releaseNotes
+      ) : (
+        <PortableText value={releaseNotes} />
+      )}
+    </div>
   </div>
 )
