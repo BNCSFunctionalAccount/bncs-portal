640ba8f10093b1a70c67165d32c332d50308195f
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-09-27 16:49:29 +0100

    add sidebar constants

---
+++ src/components/sidebar/constants.tsx
@@ -0,0 +1,12 @@
+import { DashboardIcon } from "~/assets/icons/DashboardIcon";
+import { INavItem } from "./types";
+import { HomeIcon } from "~/assets/icons/HomeIcon";
+import { TicketIcon } from "~/assets/icons/TicketIcon";
+import { LogoutIcon } from "~/assets/icons/LogoutIcon";
+
+export const SIDEBAR_NAVITEMS: INavItem[] = [
+	{ href: '/', text: 'Home', icon: <HomeIcon /> },
+	{ href: '/dashboard', text: 'Dashboard', icon: <DashboardIcon /> },
+	{ href: 'https://bncs.atlassian.net/servicedesk/customer/portal/1/user/login?destination=portal%2F1', text: 'Submit a Ticket', icon: <TicketIcon /> },
+	{ href: '/api/auth/logout', text: 'Logout', icon: <LogoutIcon />, isAnchor: true }
+]
