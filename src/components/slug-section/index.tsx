26efae74cc0e74183f7ab6f7a4463d4d03516edc
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-09-27 13:04:14 +0100

    Add release date and version, change styling

--- src/components/slug-section/index.tsx
+++ src/components/slug-section/index.tsx
@@ -2,13 +2,15 @@
 
 import { PortableText } from '@portabletext/react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useLiveQuery } from 'next-sanity/preview'
 import { FC } from 'react'
 
 import { sans, serif } from '~/assets/fonts'
 import { urlForImage } from '~/lib/sanity.image'
 import { Post, postBySlugQuery } from '~/lib/sanity.queries'
-import { formatDate } from '~/utils'
+import { formatDate, formatDateUK } from '~/utils'
+
 import { ReleaseNotes } from './sub-components/release-notes'
 
 interface ISlugSectionProps {
@@ -36,22 +38,40 @@ export const SlugSection: FC<ISlugSectionProps> = ({ staticPost }) => {
         <div className="w-full h-[200px] object-cover bg-black" />
       )}
       <div className="py-0 px-3">
-        <h1 className={`${sans.className} text-4xl my-5 mx-0 font-extrabold`}>
-          {post.title}
-        </h1>
-        <p className={`${sans.className} font-semibold text-sm mt-5`}>
-          {formatDate(post._createdAt)}
-        </p>
-        <p className={`${serif.className} text-2xl mt-0 min-h-24`}>
-          {post.description}
-        </p>
+        <div className="my-5 mx-0 border-b-2 border-lightGray p-2 flex justify-between">
+          <h1 className={`${sans.className} text-4xl font-extrabold`}>
+            {post.title}
+          </h1>
+          <Link
+            className="rounded-lg bg-deepBlue min-w-44 text-center hover:bg-opacity-75 text-white
+            py-2 px-4 hover:text-evidenOrange hover:font-bold transition-all ease-in-out duration-150"
+            href={'/dashboard/'}
+            title="Dashboard"
+          >
+            Back to Table
+          </Link>
+        </div>
+        <div className="bg-lightGray p-2 rounded my-5 hover:bg-opacity-95">
+          <p
+            className={`${sans.className} font-semibold text-sm text-gray-500`}
+          >
+            {formatDate(post._createdAt)}
+          </p>
+          <p className={`${serif.className} text-2xl mt-0 min-h-24 py-1`}>
+            {post.description}
+          </p>
+        </div>
         <div className="flex justify-between">
           <h2>Latest Release</h2>
-          <div className="text-right">
-            {formatDate(post.releaseDate.toString())}
-          </div>
+          {post.releaseDate && (
+            <div className="text-right align-bottom pt-2">
+              {formatDateUK(post.releaseDate.toString())}
+            </div>
+          )}
         </div>
-        {post.releaseNotes && <ReleaseNotes post={post} />}
+        <div className="min-h-24 w-full">
+          {post.releaseNotes && <ReleaseNotes post={post} />}
+        </div>
       </div>
     </section>
   )
