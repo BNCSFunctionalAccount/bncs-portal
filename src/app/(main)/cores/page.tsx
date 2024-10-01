1edadd2a8fcd45dad95d15f61a9387fb84b2be6e
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-10-01 17:24:10 +0100

    whoops

--- src/app/(main)/cores/page.tsx
+++ src/app/(main)/cores/page.tsx
@@ -1,9 +1,9 @@
-import { Cores } from '~/components/cores'
-
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 
+import { Cores } from '~/components/cores'
+
 export default withPageAuthRequired(
-  async function Dashboard() {
+  async function CoresPage() {
     return (
       <section className="flex flex-col h-full gap-2">
         <h1 className="text-4xl">Cores</h1>
