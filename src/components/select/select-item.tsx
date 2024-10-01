ec35cdfeb5df5135a0973d15c3696ba75ff58f23
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-10-01 11:09:58 +0100

    update filter and select

--- src/components/select/select-item.tsx
+++ src/components/select/select-item.tsx
@@ -22,14 +22,14 @@ export const SelectItem: FC<ISelectItemProps> = ({
     <Fragment key={key}>
       {item.link ? (
         <Link
-          className={`p-1 ${selected ? 'bg-slate-600 border-slate-600' : 'bg-lightGray hover:bg-slate-500 hover:border-slate-500'} hover:cursor-pointer transition-all border-b last:rounded-b`}
+          className={`p-2 ${selected ? 'bg-slate-400 border-slate-600' : 'bg-lightGray hover:bg-slate-400 hover:border-slate-400'} hover:cursor-pointer transition-all border-b last:rounded-b`}
           href={item.link}
         >
           {item.title}
         </Link>
       ) : (
         <li
-          className={`p-1 ${selected ? 'bg-slate-600 border-slate-600' : 'bg-lightGray hover:bg-slate-500 hover:border-slate-500'} hover:cursor-pointer transition-all border-b last:rounded-b`}
+          className={`p-2 ${selected ? 'bg-slate-400 border-slate-400' : 'bg-lightGray hover:bg-slate-400 hover:border-slate-400'} hover:cursor-pointer transition-all border-b last:rounded-b`}
           onClick={(e) => {
             item.clickEffect && item.clickEffect(e)
             setIsOpen(false)
