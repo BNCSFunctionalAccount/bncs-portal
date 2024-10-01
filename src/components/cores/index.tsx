2cbc49f06e549ebe8c8c3717b8de07f1d934894b
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-10-01 17:16:01 +0100

    Added some text

--- src/components/cores/index.tsx
+++ src/components/cores/index.tsx
@@ -1,3 +1,7 @@
 import { FC } from 'react'
 
-export const Cores: FC = () => <div>TODO</div>
+export const Cores: FC = () => (
+  <div className="flex w-full">
+    Welcome to the Cores page - under construction
+  </div> /*  filter, search, table? */
+)
