761b6bce50658749aa342e14dc5cea23b2225fd8
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-10-02 16:22:17 +0100

    change sidebar size

--- src/components/slug-section/index.tsx
+++ src/components/slug-section/index.tsx
@@ -25,12 +25,21 @@ export const SlugSection: FC<ISlugSectionProps> = ({ staticPost }) => {
     <section className="flex h-full w-full gap-8"> {/* Flex container with gap between columns */}
       {/* Left Column */}
       <div className="flex-1 flex flex-col gap-4"> {/* flex-1 to take up remaining space */}
+
+      <Link
+          className="rounded-lg bg-deepBlue min-w-44 text-center hover:bg-opacity-75 text-white
+            py-2 px-4 hover:text-evidenOrange hover:font-bold transition-all ease-in-out duration-150 flex items-center justify-center  h-11 w-[170px]"
+          href={'/drivers'}
+          title="Drivers"
+        >
+          Back to Table
+        </Link>
       
         <h1 className={`${sans.className} text-4xl font-extrabold mb-4`}>
           {post.title}
         </h1>
 
-        <div className="h-fit overflow-y-scroll px-3 border-l-4 border-evidenOrange pl-4 hover:pl-3 hover:border-l-8 transition-all duration-100">
+        <div className="h-fit px-3 border-l-4 border-evidenOrange pl-4 hover:pl-3 hover:border-l-8 transition-all duration-100">
           <div className="bg-lightGray p-2 rounded my-5 hover:bg-opacity-95">
             <p className={`${sans.className} font-semibold text-sm text-gray-500`}>
               {formatDate(post._createdAt)}
@@ -49,7 +58,7 @@ export const SlugSection: FC<ISlugSectionProps> = ({ staticPost }) => {
         </div>
       </div>
 
-      <div className="w-64 bg-gray-100 p-4 flex flex-col gap-4 border-l border-gray-300 mt-24 h-[500px] w-[210px]"> {/* Fixed width for right column */}
+      <div className="w-64 bg-gray-100 p-4 flex flex-col gap-4 border-l border-gray-300 mt-[153px] h-[635px] w-[210px]"> {/* Fixed width for right column */}
         <button className="bg-gray-500 text-gray-300 border-none px-5 py-2.5 cursor-not-allowed rounded-lg h-11 w-44">
             Download
           </button>
