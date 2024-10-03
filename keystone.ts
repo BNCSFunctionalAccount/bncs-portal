ab7bc840370f5d76719c3a201bbef00334688a5d
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-10-03 12:55:42 +0100

    test

--- keystone.ts
+++ keystone.ts
@@ -3,7 +3,14 @@ import { config } from '@keystone-6/core'
 export default config({
   db: {
     provider: 'postgresql',
-    url: '',
+    url: 'postgres://dbuser:dbpass@localhost:5432/keystone',
+    onConnect: async (context) => {
+      /* ... */
+    },
+    // Optional advanced configuration
+    enableLogging: true,
+    idField: { kind: 'uuid' },
+    shadowDatabaseUrl: 'postgres://dbuser:dbpass@localhost:5432/shadowdb',
   },
-  lists: {},
+  /* ... */
 })
