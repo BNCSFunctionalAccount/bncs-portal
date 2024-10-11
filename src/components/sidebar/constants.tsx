a6103234a42f5a21d02fddd5a33d5ee2421b0f65
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-10-11 13:09:01 +0100

    change name

--- src/components/sidebar/constants.tsx
+++ src/components/sidebar/constants.tsx
@@ -9,7 +9,7 @@ export const SIDEBAR_NAVITEMS: INavItem[] = [
   { href: '/', text: 'Home', icon: <HomeIcon /> },
   { href: '/drivers', text: 'Drivers', icon: <DriverIcon /> },
   { href: '/cores', text: 'Cores', icon: <CoresIcon /> },
-  { href: '/html/index.html', text: 'HTML Guide', icon: <CoresIcon />},
+  { href: '/html/index.html', text: 'BNCS Guide', icon: <CoresIcon />},
   {
     href: 'https://bncs.atlassian.net/servicedesk/customer/portal/1/user/login?destination=portal%2F1',
     text: 'Submit a Ticket',
