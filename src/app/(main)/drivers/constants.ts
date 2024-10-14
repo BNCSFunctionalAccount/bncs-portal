bb173c00829d7c4f511d0939862f805f6afa86e4
BNCSFunctionalAccount <bncs-account@eviden.com>
2024-10-14 17:23:43 +0100

    change table width

--- src/app/(main)/drivers/constants.ts
+++ src/app/(main)/drivers/constants.ts
@@ -3,7 +3,7 @@ import { IHeader } from '~/components/table/types';
 export const DRIVER_HEADERS: IHeader[] = [
   { title: 'Name', width: '250px' },
   { title: 'Version', width: '100px' },
-  {title: "Manufacturer", width: '100px'},
+  {title: "Manufacturer", width: '200px'},
   { title: 'Description', width: '550px' },
   { title: 'Availability', width: '90px' },
 ];
